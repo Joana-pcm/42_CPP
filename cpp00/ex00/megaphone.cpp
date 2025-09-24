a4098f7e5227e8c1242d9f2a74ff561d0034eb4a
jpatrici <joana.pcm@outlook.com>
2025-09-24 01:46:18 +0100

    start

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main(int ac, char** av) {
+
+	if (ac < 2) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+
+	for (int i = 1; i < ac; ++i) {
+		for (int j = 0; av[i][j]; ++j) {
+			av[i][j] = std::toupper(static_cast<unsigned char>(av[i][j]));
+		}
+		std::cout << av[i];
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
