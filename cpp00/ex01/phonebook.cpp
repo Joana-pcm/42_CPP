a642e338aaa43fc030e05d3b834b02d32959c617
Joana Patricia Cardoso Moreira <jpatrici@c1r10s4.42porto.com>
2025-10-09 19:32:23 +0100

    pb

--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include "phonebook.hpp"
 
+void init_pb(PhoneBook phone)
+{
+    phone.
+}
+
+int main(int ac, char **av)
+{
+    PhoneBook phonebook;
+    
+    init_pb(phonebook);
+}
--- cpp00/ex01/phonebook.hpp
+++ cpp00/ex01/phonebook.hpp
@@ -2,16 +2,8 @@
 #include <string>
 #include <iomanip>
 
-class phonebook {
-private:
-	struct Contact {
-		std::string firstName;
-		std::string lastName;
-		std::string nickname;
-		std::string phoneNumber;
-		std::string darkestSecret;
-	};
-
+class PhoneBook {
+public:
 	Contact contacts[8];
 	int contactCount;
 
@@ -19,4 +11,14 @@ private:
 	void searchContacts() const;
 	void displayContact(int index) const;
 	std::string formatField(const std::string& field) const;
+};
+
+class Contact
+{
+public:
+		std::string firstName;
+		std::string lastName;
+		std::string nickname;
+		std::string phoneNumber;
+		std::string darkestSecret;
 };
