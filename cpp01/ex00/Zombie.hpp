c01c3fd959200095b5cd4203bacd8e9dcfa1049e
Joana Patricia Cardoso Moreira <jpatrici@c1r9s5.42porto.com>
2025-10-20 19:24:30 +0100

    begun

--- cpp01/ex00/Zombie.hpp
+++ cpp01/ex00/Zombie.hpp
@@ -10,6 +10,8 @@ public:
 	~Zombie();
 	Zombie(std::string name);
 	void announce( void );
-	Zombie* newZombie(std::string name);
 	void randomChump(std::string name);
 };
+
+
+	Zombie* newZombie(std::string name);
