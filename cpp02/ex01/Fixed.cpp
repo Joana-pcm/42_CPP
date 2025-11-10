361b009e1222e339a49fb4ce89d83b65ab7bc04d
Joana Patricia Cardoso Moreira <jpatrici@c1r9s5.42porto.com>
2025-11-10 19:12:59 +0000

    started ex01

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,56 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _fixedPointValue(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+Fixed &Fixed::operator=(const Fixed &src)
+{
+	std::cout <<  "Copy Assignment operator called" << std::endl;
+	if (this == &src)
+		return (*this);
+	this->setRawBits(src.getRawBits());
+	return (*this);
+}
+
+Fixed::Fixed(const Fixed &src)
+{
+	this->_fixedPointValue = src._fixedPointValue;
+	std::cout << "Copy constructor called" << std::endl;
+}
+Fixed::Fixed(const int i) : _fixedPointValue(i)
+{
+}
+Fixed::Fixed(const float f) : _fixedPointValue(f)
+{
+}
+int	Fixed::toInt(void) const
+{
+	return (this->_fixedPointValue >> _fractionalBits);
+}
+
+float	Fixed::toFloat(void) const
+{
+	return (static_cast <float> (this->_fixedPointValue) / (1 << _fractionalBits));
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_fixedPointValue;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->_fixedPointValue = raw;
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &src)
+{
+	out << src.toFloat();
+	return (out);
+}
---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+
+class Fixed
+{
+	private:
+	int                 _fixedPointValue;
+	static const int    _fractionalBits = 8;
+	public:
+	Fixed();
+	~Fixed();
+	Fixed(const Fixed &src);
+	Fixed(const int i);
+	Fixed(const float f);
+	float toFloat( void ) const;
+	int toInt( void ) const;
+	Fixed &operator=(const Fixed &src);
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+};
+
+std::ostream &operator<<(std::ostream &out, const Fixed &src);
