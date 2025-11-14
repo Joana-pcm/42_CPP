ed26f0f2fa657f89502b6620056f0b1da80f1dd3
Joana Patricia Cardoso Moreira <jpatrici@c1r9s3.42porto.com>
2025-11-14 11:42:53 +0000

    changes in cpp02 ex02 main.cpp

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class ClapTrap
+{
+private:
+	std::string name;
+	
+
+public:
+	ClapTrap();
+	ClapTrap(std::string name);
+	~ClapTrap();
+};
+
+ClapTrap::ClapTrap(/* args */)
+{
+}
+
+ClapTrap::~ClapTrap()
+{
+}
