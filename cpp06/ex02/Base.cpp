22dec56e7551f4e30d08014f6d7da7e40b9f2f3b
jpatrici <joana.pcm@outlook.com>
2026-02-04 17:59:09 +0000

    fixed

--- cpp06/ex02/Base.cpp
+++ cpp06/ex02/Base.cpp
@@ -30,12 +30,11 @@ void identify(Base* p)
 
 void identify(Base& p)
 {
-	Base* ptr = &p;
-	if (dynamic_cast<A*>(ptr))
+	if (dynamic_cast<A*>(&p))
 		std::cout << "A" << std::endl;
-	else if (dynamic_cast<B*>(ptr))
+	else if (dynamic_cast<B*>(&p))
 		std::cout << "B" << std::endl;
-	else if (dynamic_cast<C*>(ptr))
+	else if (dynamic_cast<C*>(&p))
 		std::cout << "C" << std::endl;
 	else
 		std::cout << "Unknown type" << std::endl;
